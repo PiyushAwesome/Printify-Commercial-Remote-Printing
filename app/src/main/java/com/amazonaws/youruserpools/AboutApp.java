3e8a3f1783df0749ae9fa03630741d40209fc2ae
Parth Garg <parthgarg1997@gmail.com>
2019-05-11 15:08:52 +0530

    parth g

--- app/src/main/java/com/amazonaws/youruserpools/AboutApp.java
+++ app/src/main/java/com/amazonaws/youruserpools/AboutApp.java
@@ -13,7 +13,7 @@
  * CONDITIONS OF ANY KIND, express or implied. See the License
  * for the specific language governing permissions and
  * limitations under the License.
- * hello
+ * hello there
  */
 
 package com.amazonaws.youruserpools;
